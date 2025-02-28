c13baa7f3f30d548b3643e5538ea2137304c701b
IanShi1996 <ian.shi@mail.utoronto.ca>
2025-02-28 00:34:27 -0500

    MRL-Sample initial commit.

--- mrna_bench/datasets/dataset_catalog.py
+++ mrna_bench/datasets/dataset_catalog.py
@@ -2,21 +2,27 @@ from collections.abc import Callable
 
 from .benchmark_dataset import BenchmarkDataset
 from .go_mol_func import GOMolecularFunction
-from .pcg_essentiality import PCGEssentiality
-from .lncrna_essentiality import LNCRNAEssentiality
 from .rna_hl_human import RNAHalfLifeHuman
 from .rna_hl_mouse import RNAHalfLifeMouse
 from .prot_loc import ProteinLocalization
 from .mrl_sugimoto import MRLSugimoto
+from .mrl_sample import (
+    MRLSampleEGFP,
+    MRLSampleMCherry,
+    MRLSampleDesigned,
+    MRLSampleVarying
+)
 
 DATASET_CATALOG: dict[str, Callable[..., BenchmarkDataset]] = {
     "go-mf": GOMolecularFunction,
-    "pcg-ess": PCGEssentiality,
-    "lncrna-ess": LNCRNAEssentiality,
     "rnahl-human": RNAHalfLifeHuman,
     "rnahl-mouse": RNAHalfLifeMouse,
     "prot-loc": ProteinLocalization,
     "mrl-sugimoto": MRLSugimoto,
+    "mrl-sample-egfp": MRLSampleEGFP,
+    "mrl-sample-mcherry": MRLSampleMCherry,
+    "mrl-sample-designed": MRLSampleDesigned,
+    "mrl-sample-varying": MRLSampleVarying,
 }
 
 DATASET_DEFAULT_TASK: dict[str, str] = {
