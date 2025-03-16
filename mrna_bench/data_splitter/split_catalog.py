899c8eab73a9ecf31c550776c44d4b96130d496e
IanShi1996 <ian.shi@mail.utoronto.ca>
2025-03-16 00:51:30 -0400

    Added kmer splitting.

--- mrna_bench/data_splitter/split_catalog.py
+++ mrna_bench/data_splitter/split_catalog.py
@@ -1,11 +1,13 @@
 from collections.abc import Callable
 
 from mrna_bench.data_splitter.data_splitter import DataSplitter
+from mrna_bench.data_splitter.kmer_split import KMerSplitter
 from mrna_bench.data_splitter.homology_split import HomologySplitter
 from mrna_bench.data_splitter.sklearn_split import SklearnSplitter
 
 
 SPLIT_CATALOG: dict[str, Callable[..., DataSplitter]] = {
     "default": SklearnSplitter,
-    "homology": HomologySplitter
+    "homology": HomologySplitter,
+    "kmer": KMerSplitter
 }
