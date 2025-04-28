1f9dd5a3ac5bdf0ea4fadf8ffd9eec9c836428db
Fradkin <fradkinp@islogin01.mskcc.org>
2025-04-28 04:46:04 -0400

    feat: update running script

--- scripts/linear_probe/slurm/eval_model.sh
+++ scripts/linear_probe/slurm/eval_model.sh
@@ -7,45 +7,42 @@
 #SBATCH --cpus-per-task=5
 #SBATCH --mem=32G
 #SBATCH --time=6:00:00
-#SBATCH --array=6,4,2,1,0
+#SBATCH --array=0-2
 # Adjust the --array range (0-3) to match the number of model_versions minus one.
 
 # Define an array of model_versions
 model_versions=(
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.15_splice_all_basic_eutheria_siglip_norm_exp_w=0_bs_150"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_default_m30"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_default_m30"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_t4_default_m30_t6"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_t4_default_m30_t6"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_t6_default_m30_t6"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_t6_default_m30_t6"
-    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_splice_only_eutheria_0_t6_default_m30_t6"
+    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_new_splice_to_toga_mask_value2_mask_val_0.25"
+    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_new_splice_to_toga_mask_value_mask_val_-25"
+    "ssm_6t_6_512_lr0.0005_wd1e-05_mask0.3_new_splice_to_toga_combined_load_new_load_new_combined_s:toga"
+)
+ckpt_names=(
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
+    "epoch=10-step=20000.ckpt"
 )
 # boolean input
 mask_out_splice_tracks=(
     "False"
     "False"
     "False"
-    "True"
-    "False"
-    "True"
-    "False"
-    "True"
 )
 mask_out_cds_tracks=(
     "False"
     "False"
     "False"
-    "False"
-    "False"
-    "False"
-    "False"
-    "True"
 )
 # Select the model_version based on the SLURM_ARRAY_TASK_ID
 model_version=${model_versions[$SLURM_ARRAY_TASK_ID]}
 mask_out_splice_track=${mask_out_splice_tracks[$SLURM_ARRAY_TASK_ID]}
 mask_out_cds_track=${mask_out_cds_tracks[$SLURM_ARRAY_TASK_ID]}
+ckpt_name=${ckpt_names[$SLURM_ARRAY_TASK_ID]}
 
 echo "Selected model_version: $model_version"
 
@@ -61,6 +58,6 @@ cd /home/fradkinp/Documents/01_projects/mRNABench/scripts/linear_probe
 
 python linear_probe_all.py --model_name Orthrus \
     --model_version ${model_version} \
-    --ckpt_name epoch=22-step=20000.ckpt \
+    --ckpt_name ${ckpt_name} \
     --mask_out_splice_track ${mask_out_splice_track} \
     --mask_out_cds_track ${mask_out_cds_track}
