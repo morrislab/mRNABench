bd243157d46b47cd5b7b934002d4c935e5d10286
Taykhoom Dalal <tid4007@med.cornell.edu>
2026-02-13 20:20:42 -0500

    test omnigenome separately

--- precommit.sh
+++ precommit.sh
@@ -2,5 +2,5 @@ flake8 mrna_bench --ignore=D100,D104
 mypy mrna_bench --ignore-missing-imports
 
 # Require GPU
-pytest -m "not omnigenome"
-pytest -m omnigenome
+pytest -k "not omnigenome"
+pytest -k "omnigenome"
